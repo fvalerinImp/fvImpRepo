0f6f50875af1a77867649f5254b4f1b2216ecd2a
Federico Valerin <fvalerin@Administrators-MacBook-Pro.local>
2017-07-21 16:48:10 -0600

    Sube Quiz4Gatos copy

---
+++ OtrosContenido/Quiz4Gatos copy/Quiz4Gatos/CatsListViewController.swift
@@ -0,0 +1,123 @@
+//
+//  CatsListViewController.swift
+//  Quiz4Gatos
+//
+//  Created by Federico Valerin on 5/30/17.
+//  Copyright © 2017 Federico Valerin. All rights reserved.
+//
+
+import UIKit
+
+class CatsListViewController: UIViewController {
+
+    var arrCats = [clsCat]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        CargarGatos()
+        tableView.registerCustomCell(identifier: CatCustomTableViewCell.getTableViewCellIdentifier())
+        
+        self.title = "Cats"
+        createAddButton()
+    }
+    
+    func CargarGatos() {
+        
+        let gato = clsCat(pNombre: "Minino")
+        
+        arrCats = [gato]
+    }
+
+    func createAddButton(){
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addAction))
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    func addAction() {
+        let viewController = storyboard!.instantiateViewController(withIdentifier: AddCatViewController.getViewControllerIdentifier()) as! AddCatViewController
+        
+        viewController.delegado = self
+        
+        navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CatsListViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (arrCats.count)
+//        guard let lperros = lstPerros else {
+//            return 0
+//        }
+//        return lperros.count
+//        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CatCustomTableViewCell.getTableViewCellIdentifier()) as! CatCustomTableViewCell
+        
+        cell.AsignarCelda(pGato: (arrCats[ indexPath.row ]) )
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 60
+        
+    }
+}
+
+extension CatsListViewController: clsAddCatProtocol {
+    func agregarObjeto( pObjeto: clsCat ) {
+        
+//        var nContador = 0
+//        if lstPerros != nil {
+//            if lstPerros?.count != nil {
+//                nContador = (lstPerros?.count)!
+//            }
+//        }
+        
+//        pObjeto.Orden = nContador + 1
+        //self.arrPerros.append( pObjeto )
+  //      RealmManager.addObjectToRealm(realmObject: pObjeto)
+    
+        arrCats.append( pObjeto )
+        tableView.reloadData()
+    }
+    
+}
+
+
+
+
+
+
+
